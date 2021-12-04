959374cb50e8a6d7e1b5f0afd98668d69dfbc95a
Doug Saylor <doug@saylorsolutions.com>
2021-12-04 00:05:38 -0600

    Finished day 3

--- aoc3/main.go
+++ aoc3/main.go
@@ -27,6 +27,13 @@ func (s DigitStats) mostCommonDigit() int {
 	return 1
 }
 
+func (s DigitStats) mostCommonTieOne() int {
+	if s.Zeros == s.Ones {
+		return 1
+	}
+	return s.mostCommonDigit()
+}
+
 func (s DigitStats) leastCommonDigit() int {
 	if s.mostCommonDigit() == 0 {
 		return 1
@@ -34,22 +41,41 @@ func (s DigitStats) leastCommonDigit() int {
 	return 0
 }
 
-func main() {
-	inputs := strings.Split(input, "\n")
-	gamma, epsilon := getGammaEpsilon(inputs)
-	fmt.Println("Power consumption is:", gamma*epsilon)
+func (s DigitStats) leastCommonTieZero() int {
+	if s.Zeros == s.Ones {
+		return 0
+	}
+	return s.leastCommonDigit()
 }
 
-func getGammaEpsilon(inputs []string) (gamma uint64, epsilon uint64) {
+func main() {
+	rawInput := strings.Split(input, "\n")
+	digits := len(rawInput[0])
+	inputs := parseRawInputs(rawInput)
+	gamma, epsilon := getGammaEpsilon(inputs, digits)
+	// Gamma: 805, Epsilon: 3290, Power consumption: 2648450
+	fmt.Printf("Gamma: %d, Epsilon: %d, Power consumption: %d\n", gamma, epsilon, gamma*epsilon)
+	o2 := filterMostCommon(inputs, digits)
+	co2 := filterLeastCommon(inputs, digits)
+	fmt.Printf("Oxygen generation: %d, Co2 scrubbing: %d, Life support: %d\n", o2, co2, o2 * co2)
+}
+
+func getGammaEpsilon(inputs []uint64, digits int) (gamma uint64, epsilon uint64) {
+	dataMap := getDigitStats(inputs, digits)
+	for offset, stats := range dataMap {
+		gamma += offset * uint64(stats.mostCommonDigit())
+	}
+
+	epsilon = gamma ^ fScreen
+	epsilon = epsilon & (fScreen >> (64 - digits))
+
+	return gamma, epsilon
+}
+
+func getDigitStats(inputs []uint64, digits int) map[uint64]*DigitStats {
 	dataMap := map[uint64]*DigitStats{}
-	digits := len(inputs[0])
 
-	for _, in := range inputs {
-		num, err := strconv.ParseUint(in, 2, 64)
-		if err != nil {
-			log.Panicf("Input '%s' cannot be parsed as a binary number: %v\n", in, err)
-		}
-
+	for _, num := range inputs {
 		for offset := digits - 1; offset >= 0; offset-- {
 			offsetVal := uint64(1) << offset
 			data, ok := dataMap[offsetVal]
@@ -63,14 +89,60 @@ func getGammaEpsilon(inputs []string) (gamma uint64, epsilon uint64) {
 				data.Zeros++
 			}
 		}
-
 	}
-	for offset, stats := range dataMap {
-		gamma += offset * uint64(stats.mostCommonDigit())
+	return dataMap
+}
+
+func parseRawInputs(inputs []string) []uint64 {
+	list := make([]uint64, len(inputs))
+	for i, in := range inputs {
+		num, err := strconv.ParseUint(in, 2, 64)
+		if err != nil {
+			log.Panicf("Input '%s' cannot be parsed as a binary number: %v\n", in, err)
+		}
+		list[i] = num
 	}
+	return list
+}
 
-	epsilon = gamma ^ fScreen
-	epsilon = epsilon & (fScreen >> (64 - digits))
+func filterMostCommon(inputs []uint64, digits int) uint64 {
+	dataMap := getDigitStats(inputs, digits)
+	digitScreen := uint64(1) << (digits - 1)
+	mostCommon := dataMap[digitScreen].mostCommonTieOne()
 
-	return gamma, epsilon
+	var remaining []uint64
+	for _, in := range inputs {
+		if mostCommon == 1 {
+			if in & digitScreen > 0 {
+				remaining = append(remaining, in)
+			}
+		} else if in & digitScreen == 0 {
+			remaining = append(remaining, in)
+		}
+	}
+	if len(remaining) > 1 {
+		return filterMostCommon(remaining, digits - 1)
+	}
+	return remaining[0]
+}
+
+func filterLeastCommon(inputs []uint64, digits int) uint64 {
+	dataMap := getDigitStats(inputs, digits)
+	digitScreen := uint64(1) << (digits - 1)
+	leastCommon := dataMap[digitScreen].leastCommonTieZero()
+
+	var remaining []uint64
+	for _, in := range inputs {
+		if leastCommon == 1 {
+			if in & digitScreen > 0 {
+				remaining = append(remaining, in)
+			}
+		} else if in & digitScreen == 0 {
+			remaining = append(remaining, in)
+		}
+	}
+	if len(remaining) > 1 {
+		return filterLeastCommon(remaining, digits - 1)
+	}
+	return remaining[0]
 }
--- aoc3/main_test.go
+++ aoc3/main_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestGetGammaEpsilon(t *testing.T) {
-	inputs := []string{
+var (
+	testInputs = []string{
 		"00100",
 		"11110",
 		"10110",
@@ -20,9 +20,25 @@ func TestGetGammaEpsilon(t *testing.T) {
 		"00010",
 		"01010",
 	}
+)
 
-	gamma, epsilon := getGammaEpsilon(inputs)
+func TestGetGammaEpsilon(t *testing.T) {
+	numInputs := parseRawInputs(testInputs)
+	assert.Equal(t, len(testInputs), len(numInputs))
+	gamma, epsilon := getGammaEpsilon(numInputs, 5)
 	assert.Equal(t, uint64(22), gamma)
 	assert.Equal(t, uint64(9), epsilon)
 	assert.Equal(t, uint64(198), epsilon*gamma)
+}
+
+func TestLifeSupport(t *testing.T) {
+	numInputs := parseRawInputs(testInputs)
+	o2 := filterMostCommon(numInputs, 5)
+	assert.Equal(t, uint64(23), o2)
+
+	co2 := filterLeastCommon(numInputs, 5)
+	assert.Equal(t, uint64(10), co2)
+
+	lifeSupport := o2 * co2
+	assert.Equal(t, uint64(230), lifeSupport)
 }
